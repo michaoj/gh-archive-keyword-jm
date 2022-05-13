48ae495581e3110604e4def9ad3264dbd6fb13d8
Joachim Martin <joachim.martin@emilfrey.fr>
2022-05-13 08:27:37 +0200

    feat(composer) : add monolog
    - Add monolog bundle
    - Create a channel to log all import informations
    
    Reference : None

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Fresh\DoctrineEnumBundle\FreshDoctrineEnumBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
